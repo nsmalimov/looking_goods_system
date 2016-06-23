48bbd19c804b86f1626d660d1f67e4fe4a76c488
Nurislam Alimov <nalimov@sardinasystems.com>
2016-06-23 18:27:08 +0300

    add work with cache

--- main_loader.php
+++ main_loader.php
@@ -1,51 +1,29 @@
 <?php
 
+$memcache = new Memcache;
+$memcache->connect('localhost', 11211) or exit("Невозможно подключиться к серверу Memcached");
+
+$count = intval($memcache->get("count"));
+
 $num = $_POST['num'];
 
-$num_under = $num - 100;
+$need_pages =  $count / 100;
 
-if ($num_under == 0)
-{
-    $num_under = 1;
-}
+$page_current = $num / 100;
 
-$dbhost = 'localhost:3306';
-$dbuser = 'root';
-$dbpass = 've;br';
-$conn = mysql_connect($dbhost, $dbuser);
-if(! $conn )
-{
-    die('Could not connect: ' . mysql_error());
-}
+$ids_need_arr = $memcache->get("ids_sorted_id_" . $num);
 
-$sql = 'SELECT id, id_num, cost, title, url_image, description FROM goods WHERE id >= ' . $num_under . ' AND id <=' . $num;
-
-mysql_select_db('goods_data');
-$retval = mysql_query( $sql, $conn );
-
-if(! $retval )
-{
-    die('Could not get data: ' . mysql_error());
-}
-
-
-
-//function select() {
-//    echo "The select function is called.";
-//    exit;
-//}
-//
-//function insert() {
-//    echo "The insert function is called.";
-//    exit;
-//}
-
-echo "<div class='section'><div class='container'>";
+$ids_need_arr = explode(",", $ids_need_arr);
 
 $counter = 0;
 
-while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+for ($i=0;$i<count($ids_need_arr);$i++)
 {
+    $row = $memcache->get($ids_need_arr[$i]);
+    $row = str_replace("'", "\"", $row);
+
+    $row = json_decode($row);
+
     if ($counter % 4 == 0)
     {
         echo "</div>";
@@ -53,40 +31,55 @@ while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
     }
 
     echo "<div class='col-md-2'>
-                <img src='{$row['url_image']}'
+                <img src='{$row->{'url_image'}}'
                      class='img-responsive'>
-                <h2>{$row['title']}</h2>
-                <p>num: {$row['id_num']}</p>
-                <p>description: {$row['description']}</p>
-                <p>cost: {$row['cost']}</p>
+                <h2>{$row->{'title'}}</h2>
+                <p>num: {$ids_need_arr[$i]}</p>
+                <p>description: {$row->{'description'}}</p>
+                <p>cost: {$row->{'cost'}}</p>
             </div>";
 
     $counter ++;
 }
 
-echo "</div></div>";
+//while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+//{
+//    if ($counter % 4 == 0)
+//    {
+//        echo "</div>";
+//        echo "<div class='row'>";
+//    }
+//
+//    echo "<div class='col-md-2'>
+//                <img src='{$row['url_image']}'
+//                     class='img-responsive'>
+//                <h2>{$row['title']}</h2>
+//                <p>num: {$row['id_num']}</p>
+//                <p>description: {$row['description']}</p>
+//                <p>cost: {$row['cost']}</p>
+//            </div>";
+//
+//    $counter ++;
+//}
+//
+//echo "</div></div>";
 
-$result = mysql_query("SELECT count(*) from goods;");
-$need_pages =  mysql_result($result, 0) / 100;
-
-$page_current = $num / 100;
 
 echo "<div id=\"page-selection\"></div>
 <script type=\"text/javascript\" src=\"http://localhost/looking_goods_system/resources/js/main_page.js\"></script>
 <script>
-    // init bootpag
     $('#page-selection').bootpag({
         total: $need_pages,
         page: $page_current,
         maxVisible: 15
-        
+
     }).on(\"page\", function (event, num) {
-    
+
         showAllFunc(num * 100);
     });
 </script>";
 
-mysql_close($conn);
+$memcache->close();
 
 ?>
 
